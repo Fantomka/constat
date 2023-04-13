c9001e9e6c49350839d93a89b78ffbb968596f37
Симаньков Артём <avs@techokad.ru>
2023-04-13 23:56:41 +0300

    Initial commit

---
+++ src/test/java/ru/autoopt/constat/ConstatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.autoopt.constat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
