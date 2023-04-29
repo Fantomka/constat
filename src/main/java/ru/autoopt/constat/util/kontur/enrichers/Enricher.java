1be8c35aa8f9f27b8228517eee205430b98d9407
Simankov Artem <simankovartem17@gmail.com>
2023-04-29 16:52:12 +0300

    Changes definition of enrichers

--- src/main/java/ru/autoopt/constat/util/kontur/enrichers/Enricher.java
+++ src/main/java/ru/autoopt/constat/util/kontur/enrichers/Enricher.java
@@ -1,12 +1,9 @@
 package ru.autoopt.constat.util.kontur.enrichers;
 
 import ru.autoopt.constat.dto.ContractorDTO;
-import ru.autoopt.constat.util.kontur.KonturConnector;
 
 public interface Enricher {
 
-    String getApiMethod();
-
-    ContractorDTO enrich(ContractorDTO contractorDTO, KonturConnector connector);
+    ContractorDTO enrich(ContractorDTO contractorDTO);
 
 }
