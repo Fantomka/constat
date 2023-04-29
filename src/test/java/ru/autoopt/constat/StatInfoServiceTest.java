1e1067306757849bc710373bbfbced3c911dc3c8
Simankov Artem <simankovartem17@gmail.com>
2023-04-29 21:20:05 +0300

    Added test for stat info getter

--- src/test/java/ru/autoopt/constat/StatInfoServiceTest.java
+++ src/test/java/ru/autoopt/constat/StatInfoServiceTest.java
@@ -2,8 +2,11 @@ package ru.autoopt.constat;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.autoopt.constat.services.statinfo.StatInfoType;
 import ru.autoopt.constat.services.statinfo.StaticInfoService;
+
 
 import java.io.IOException;
 
@@ -11,10 +14,10 @@ import java.io.IOException;
 public class StatInfoServiceTest {
 
     @Test
-    public void konturGetRequestTest() throws IOException {
+    public void statInfoGetValueTest() throws IOException {
 
         StaticInfoService staticInfoService = new StaticInfoService(new ObjectMapper());
 
-        staticInfoService.getInfo();
+        Assertions.assertEquals(21, staticInfoService.getValue(StatInfoType.REGION_RU, "04", Integer.class));
     }
 }
