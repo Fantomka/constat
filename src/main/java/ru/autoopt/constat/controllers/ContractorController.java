a312076958d9b7a93ee3e2745eb903c9405a5c01
ELMAN SELIMOV <elman221199@gmail.com>
2023-04-30 22:48:36 +0300

    withdrawal related changes

--- src/main/java/ru/autoopt/constat/controllers/ContractorController.java
+++ src/main/java/ru/autoopt/constat/controllers/ContractorController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.autoopt.constat.dto.ContractorDTO;
 import ru.autoopt.constat.models.Contractor;
 import ru.autoopt.constat.services.ContractorService;
 import ru.autoopt.constat.util.validators.ContractorValidator;
@@ -30,18 +31,19 @@ public class ContractorController {
     }
 
     @GetMapping("/new")
-    public String newPerson(@ModelAttribute("contractor") Contractor contractor) {
+    public String newPerson(@ModelAttribute("contractor") ContractorDTO contractor) {
         return "contractors/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("contractor") @Valid Contractor contractor, BindingResult bindingResult) {
+    public String create(@ModelAttribute("contractor") @Valid ContractorDTO contractor, BindingResult bindingResult) {
         contractorValidator.validate(contractor, bindingResult);
 
         if (bindingResult.hasErrors())
             return "contractors/new";
 
-        contractorService.save(contractor);
+        contractorService.counterpartyVerification(contractor);
+        //contractorService.save(contractor);
         return "redirect:contractors";
     }
 }
--- src/main/java/ru/autoopt/constat/services/ContractorService.java
+++ src/main/java/ru/autoopt/constat/services/ContractorService.java
@@ -9,7 +9,11 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.autoopt.constat.dto.ContractorDTO;
 import ru.autoopt.constat.models.Contractor;
 import ru.autoopt.constat.repositories.ContractorRepository;
+import ru.autoopt.constat.services.calculators.Calculator;
 import ru.autoopt.constat.services.calculators.RateCalculatorService;
+import ru.autoopt.constat.services.calculators.StatusCode;
+import ru.autoopt.constat.services.statinfo.StatInfoType;
+import ru.autoopt.constat.services.statinfo.StaticInfoService;
 
 import java.util.*;
 
@@ -20,6 +24,8 @@ public class ContractorService {
 
     private final ContractorRepository contractorRepository;
     private final ModelMapper modelMapper;
+    private final Calculator calculator;
+    private final StaticInfoService staticInfoService;
 
     public List<ContractorDTO> index() {
         List<Contractor> index = contractorRepository.findAll();
@@ -31,6 +37,39 @@ public class ContractorService {
 
     public Optional<Contractor> getContractorByINN(String INN) {
         return contractorRepository.findByINN(INN);
+    }
+
+    public void counterpartyVerification(ContractorDTO contractorDTO) {
+
+        int limitPeriod = 0;
+        double limitAmount = 0;
+        String returnedResponse = "";
+
+        StatusCode statusCode = calculator.calculate(contractorDTO);
+        int periodByAreaCode = staticInfoService.getValue(StatInfoType.REGION_RU, contractorDTO.getINN().substring(0, 2), Integer.class);
+
+        if(contractorDTO.getRate() >= 11 && contractorDTO.getRate() <= 20) {
+            limitPeriod = (periodByAreaCode > 7) ? periodByAreaCode : 7;
+            limitAmount = (contractorDTO.getRevenue()/365) * (limitPeriod/4);
+        } else if (contractorDTO.getRate() >= 21 && contractorDTO.getRate() <= 35) {
+            limitPeriod = (periodByAreaCode > 14) ? periodByAreaCode : 14;
+            limitAmount = (contractorDTO.getRevenue()/365) * (limitPeriod/4);
+        } else if (contractorDTO.getRate() >= 36 && contractorDTO.getRate() <= 50) {
+            limitPeriod = (periodByAreaCode > 45) ? periodByAreaCode : 45;
+            limitAmount = (contractorDTO.getRevenue()/365) * (limitPeriod/4);
+        } else if (contractorDTO.getRate() <= 10) {
+            returnedResponse += "Кредит не разрешен - только полная предоплата ! \n";
+            if (statusCode == StatusCode.REORGANIZATION) {
+                returnedResponse += "Компания находиться в стадии реорганизации \n";
+            } else if (statusCode == StatusCode.FOUND_DATE_LATE) {
+                returnedResponse += "Компания образована после 01.07.2022 \n";
+            } else if (statusCode == StatusCode.MASS_ORG_ADDRESS) {
+                returnedResponse += "Компания имеет несколько ЮР адресов\n";
+            }
+        }
+
+        returnedResponse += "Кредит разрешен на " + limitPeriod + " дней, сумма - " + limitAmount + "\n";
+
     }
 
     @Transactional
